2c8dfa03444ac8563157faeff3354ba0e907173d
Gravit <gravit.min@ya.ru>
2019-11-26 21:39:52 +0700

    [FIX] UpdateOverlay UI

--- src/main/java/pro/gravit/launcher/client/gui/overlay/UpdateOverlay.java
+++ src/main/java/pro/gravit/launcher/client/gui/overlay/UpdateOverlay.java
@@ -1,18 +1,31 @@
 package pro.gravit.launcher.client.gui.overlay;
 
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.shape.Circle;
 import pro.gravit.launcher.client.gui.JavaFXApplication;
 import pro.gravit.launcher.client.gui.raw.AbstractOverlay;
 
 import java.io.IOException;
 
 public class UpdateOverlay extends AbstractOverlay {
+    public ProgressBar progressBar;
+    public Circle[] phases;
+    public Label speed;
     public UpdateOverlay(JavaFXApplication application) throws IOException {
         super("overlay/update/update.fxml", application);
     }
 
     @Override
     protected void doInit() throws IOException {
-
+        progressBar = (ProgressBar) pane.lookup("#progress");
+        phases = new Circle[5];
+        for(int i=1;i<=5;++i)
+        {
+            phases[i-1] = (Circle) pane.lookup("#phase".concat(Integer.toString(i)));
+        }
+        speed = (Label) pane.lookup("#speed");
     }
 
     @Override
