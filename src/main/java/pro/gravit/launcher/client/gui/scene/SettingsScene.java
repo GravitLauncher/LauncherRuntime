87e994f26726ca537ea19e2f498a08d7566bf838
Gravit <gravit.min@ya.ru>
2019-11-29 01:30:45 +0700

    [REFACTOR] ramSlider

--- src/main/java/pro/gravit/launcher/client/gui/scene/SettingsScene.java
+++ src/main/java/pro/gravit/launcher/client/gui/scene/SettingsScene.java
@@ -7,9 +7,11 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.stage.Stage;
 import pro.gravit.launcher.client.DirBridge;
+import pro.gravit.launcher.client.FunctionalBridge;
 import pro.gravit.launcher.client.gui.JavaFXApplication;
 import pro.gravit.launcher.client.gui.helper.LookupHelper;
 import pro.gravit.launcher.client.gui.raw.AbstractScene;
+import pro.gravit.utils.helper.JVMHelper;
 import pro.gravit.utils.helper.LogHelper;
 
 public class SettingsScene extends AbstractScene {
@@ -28,6 +30,18 @@ public class SettingsScene extends AbstractScene {
         Slider ramSlider = (Slider) layout.lookup("#ramSlider");
         Label ramLabel = (Label) layout.lookup("#settingsBackground").lookup("#ramLabel");
         ramLabel.setText(Integer.toString(application.settings.ram));
+        ramSlider.setMax(FunctionalBridge.getJVMTotalMemory());
+        ramSlider.setSnapToTicks(true);
+        ramSlider.setShowTickMarks(true);
+        ramSlider.setShowTickLabels(true);
+        ramSlider.setMinorTickCount(3);
+        ramSlider.setMajorTickUnit(1024);
+        ramSlider.setBlockIncrement(1024);
+        ramSlider.setValue(application.settings.ram);
+        ramSlider.valueProperty().addListener((o, ov, nv) -> {
+            application.settings.ram = nv.intValue();
+            ramLabel.setText(application.settings.ram <= 0 ? "Auto" : application.settings.ram + " MiB");
+        });
         Hyperlink updateDirLink = LookupHelper.lookup(layout, "#dirLabel", "#patch");
         updateDirLink.setText(DirBridge.dirUpdates.toAbsolutePath().toString());
     }
