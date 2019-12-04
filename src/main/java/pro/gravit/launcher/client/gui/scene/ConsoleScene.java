8f6cd1252e2904ed635800e9f06553608f214027
Gravit <gravit.min@ya.ru>
2019-12-04 22:21:50 +0700

    [FEATURE] LauncherInfo

--- src/main/java/pro/gravit/launcher/client/gui/scene/ConsoleScene.java
+++ src/main/java/pro/gravit/launcher/client/gui/scene/ConsoleScene.java
@@ -3,6 +3,7 @@ package pro.gravit.launcher.client.gui.scene;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.control.ButtonBase;
+import javafx.scene.control.Labeled;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -11,6 +12,8 @@ import pro.gravit.launcher.client.gui.JavaFXApplication;
 import pro.gravit.launcher.client.gui.raw.AbstractScene;
 import pro.gravit.launcher.client.gui.raw.ContextHelper;
 import pro.gravit.launcher.managers.ConsoleManager;
+import pro.gravit.utils.Version;
+import pro.gravit.utils.helper.JVMHelper;
 import pro.gravit.utils.helper.LogHelper;
 
 public class ConsoleScene extends AbstractScene {
@@ -32,6 +35,7 @@ public class ConsoleScene extends AbstractScene {
         LogHelper.addOutput(this::append, LogHelper.OutputTypes.PLAIN);
         commandLine.setOnAction(this::send);
         ((ButtonBase)layout.lookup("#send")).setOnAction(this::send);
+        ((Labeled)layout.lookup("#version")).setText(getMiniLauncherInfo());
     }
     public void send(ActionEvent ignored)
     {
@@ -52,5 +56,16 @@ public class ConsoleScene extends AbstractScene {
                 output.deleteText(0, REMOVE_LENGTH);
             output.appendText(text.concat("\n"));
         });
+    }
+
+    public static String getLauncherInfo()
+    {
+        return String.format("Launcher %s | Java %d(%s %s) x%d | %s x%d", Version.getVersion().toString(), JVMHelper.JVM_VERSION, JVMHelper.RUNTIME_MXBEAN.getVmName(), System.getProperty("java.version"), JVMHelper.JVM_BITS,
+                JVMHelper.OS_TYPE.name(),JVMHelper.OS_BITS);
+    }
+    public static String getMiniLauncherInfo()
+    {
+        return String.format("Launcher %s | Java %d(%s) x%d | %s x%d", Version.getVersion().toString(), JVMHelper.JVM_VERSION, System.getProperty("java.version"), JVMHelper.JVM_BITS,
+                JVMHelper.OS_TYPE.name(), JVMHelper.OS_BITS);
     }
 }
--- src/main/java/pro/gravit/launcher/client/gui/scene/SettingsScene.java
+++ src/main/java/pro/gravit/launcher/client/gui/scene/SettingsScene.java
@@ -1,10 +1,9 @@
 package pro.gravit.launcher.client.gui.scene;
 
 import javafx.scene.Node;
-import javafx.scene.control.ButtonBase;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import pro.gravit.launcher.client.DirBridge;
 import pro.gravit.launcher.client.FunctionalBridge;
@@ -13,8 +12,11 @@ import pro.gravit.launcher.client.gui.helper.LookupHelper;
 import pro.gravit.launcher.client.gui.raw.AbstractScene;
 import pro.gravit.launcher.client.gui.stage.ConsoleStage;
 
+import java.util.function.Consumer;
+
 public class SettingsScene extends AbstractScene {
     public Node layout;
+    public Pane componentList;
     public SettingsScene(JavaFXApplication application) {
         super("scenes/settings/settings.fxml", application);
     }
@@ -22,6 +24,7 @@ public class SettingsScene extends AbstractScene {
     @Override
     protected void doInit() throws Exception {
         layout = LookupHelper.lookup(scene.getRoot(),  "#settingsPane");
+        componentList = (Pane) ((ScrollPane)layout.lookup("#settingslist")).getContent();
         sceneBaseInit(layout);
         ((ButtonBase)layout.lookup("#apply")).setOnAction((e) -> {
             contextHelper.runCallback(() -> application.setMainScene(application.gui.serverMenuScene)).run();
@@ -50,5 +53,27 @@ public class SettingsScene extends AbstractScene {
         });
         Hyperlink updateDirLink = LookupHelper.lookup(layout, "#dirLabel", "#patch");
         updateDirLink.setText(DirBridge.dirUpdates.toAbsolutePath().toString());
+        add("Debug", "debug mode", application.settings.debug, (value) -> application.settings.debug=value);
+        add("Test", "test mode", application.settings.debug, (value) -> application.settings.debug=value);
+    }
+
+    public void add(String name, String description, boolean value, Consumer<Boolean> onChanged)
+    {
+        Pane container = new Pane();
+        container.getStyleClass().add("settingsContainer");
+        CheckBox checkBox = new CheckBox();
+        checkBox.setSelected(value);
+        checkBox.getStyleClass().add("settingsCheckbox");
+        checkBox.setText(name);
+        Text desc = new Text();
+        desc.setText(description);
+        desc.getStyleClass().add("settingsDescription");
+        container.getChildren().add(checkBox);
+        container.getChildren().add(desc);
+
+        checkBox.setOnAction((e) -> {
+            onChanged.accept(checkBox.isSelected());
+        });
+        componentList.getChildren().add(container);
     }
 }
