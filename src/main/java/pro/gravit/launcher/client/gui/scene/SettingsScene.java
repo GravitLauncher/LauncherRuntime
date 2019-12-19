3b0e9d1aba855273ed4ab3628ba0893400630463
Gravit <gravit.min@ya.ru>
2019-12-19 12:15:47 +0700

    [FEATURE] Choose update directory

--- src/main/java/pro/gravit/launcher/client/gui/scene/SettingsScene.java
+++ src/main/java/pro/gravit/launcher/client/gui/scene/SettingsScene.java
@@ -8,12 +8,15 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.DirectoryChooser;
 import pro.gravit.launcher.client.DirBridge;
 import pro.gravit.launcher.client.gui.JavaFXApplication;
 import pro.gravit.launcher.client.gui.helper.LookupHelper;
 import pro.gravit.launcher.client.gui.raw.AbstractScene;
 import pro.gravit.launcher.client.gui.stage.ConsoleStage;
 
+import java.io.File;
+import java.nio.file.Path;
 import java.util.function.Consumer;
 
 public class SettingsScene extends AbstractScene {
@@ -55,6 +58,16 @@ public class SettingsScene extends AbstractScene {
         });
         Hyperlink updateDirLink = LookupHelper.lookup(layout, "#dirLabel", "#patch");
         updateDirLink.setText(DirBridge.dirUpdates.toAbsolutePath().toString());
+        ((ButtonBase)layout.lookup("#changeDir")).setOnAction((e) -> {
+            DirectoryChooser directoryChooser = new DirectoryChooser();
+            directoryChooser.setTitle("Сменить директорию загрузок");
+            directoryChooser.setInitialDirectory(DirBridge.dir.toFile());
+            Path newDir = directoryChooser.showDialog(application.getMainStage().stage).toPath().toAbsolutePath();
+            DirBridge.dirUpdates = newDir;
+            application.runtimeSettings.updatesDirPath = newDir.toString();
+            application.runtimeSettings.updatesDir = newDir;
+            updateDirLink.setText(application.runtimeSettings.updatesDirPath);
+        });
         add("Debug", "debug mode", application.runtimeSettings.debug, (value) -> application.runtimeSettings.debug=value);
         add("Auto enter", "Auto join to server", application.runtimeSettings.autoEnter, (value) -> application.runtimeSettings.autoEnter=value);
         add("Fullscreen", "Show MineCraft client in full screen mode", application.runtimeSettings.fullScreen, (value) -> application.runtimeSettings.fullScreen=value);
