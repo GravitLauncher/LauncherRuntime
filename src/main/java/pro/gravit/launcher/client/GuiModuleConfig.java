0c8284d64e5f798c41e8139d6595c491407d20f9
Георгий Торопов <kotgeorgy01@gmail.com>
2020-04-22 11:26:11 +0600

    [FEATURE] Загрузка лога на Hastebin

--- src/main/java/pro/gravit/launcher/client/GuiModuleConfig.java
+++ src/main/java/pro/gravit/launcher/client/GuiModuleConfig.java
@@ -7,11 +7,14 @@ public class GuiModuleConfig {
     public String createAccountURL;
     @LauncherInject(value = "modules.javaruntime.forgotpassurl")
     public String forgotPassURL;
+    @LauncherInject(value = "modules.javaruntime.hastebinserver")
+    public String hastebinServer;
 
     public static Object getDefault() {
         GuiModuleConfig config = new GuiModuleConfig();
         config.createAccountURL = "https://gravit.pro/createAccount.php";
         config.forgotPassURL = "https://gravit.pro/fogotPass.php";
+        config.hastebinServer = "https://hasteb.in";
         return config;
     }
 }
