4a60bc80039c41cccfce59f4125c0ce5b7e96b60
Gravit <gravit.min@ya.ru>
2020-06-17 17:36:08 +0700

    [FEATURE] Ловля ошибок горе-владельцев проектов

--- src/main/java/pro/gravit/launcher/client/JavaRuntimeModule.java
+++ src/main/java/pro/gravit/launcher/client/JavaRuntimeModule.java
@@ -26,8 +26,19 @@ public class JavaRuntimeModule extends LauncherModule {
     }
 
     private static void noJavaFxAlert() {
-        String message = String.format("Библиотеки JavaFX не найдены. У вас %s(x%d) ОС %s(x%d). Java %s. Установите Java с поддержкой JavaFX, например OracleJRE 8 x%d с официального сайта.\nЕсли вы не можете решить проблему самостоятельно обратитесь к администрации своего проекта", JVMHelper.RUNTIME_MXBEAN.getVmName(),
-                JVMHelper.JVM_BITS, JVMHelper.OS_TYPE.name, JVMHelper.OS_BITS, JVMHelper.RUNTIME_MXBEAN.getSpecVersion(), JVMHelper.OS_BITS);
+        String message = String.format("Библиотеки JavaFX не найдены. У вас %s(x%d) ОС %s(x%d). Java %s. Установите Java с поддержкой JavaFX, например OracleJRE 8 x%d с официального сайта.\n%s\nЕсли вы не можете решить проблему самостоятельно обратитесь к администрации своего проекта", JVMHelper.RUNTIME_MXBEAN.getVmName(),
+                JVMHelper.JVM_BITS, JVMHelper.OS_TYPE.name, JVMHelper.OS_BITS, JVMHelper.RUNTIME_MXBEAN.getSpecVersion(), JVMHelper.OS_BITS, JVMHelper.OS_TYPE == JVMHelper.OS.MUSTDIE ? "Убедитесь что вы запускаете с правильной Java(ПКМ->Открыть с помощью->Java 8)" : "Убедитесь, что по умолчанию стоит запуск с подходяшей Java и установлен openjfx той же версии");
+        JOptionPane.showMessageDialog(null, message, "GravitLauncher", JOptionPane.ERROR_MESSAGE);
+    }
+
+    public static void noLocaleAlert(String file) {
+        String message = String.format("Не найден файл %s при инициализации GUI. Дальнейшая работа невозможна.\nУбедитесь что все файлы дизайна лаунчера присутствуют в папке runtime при сборке лаунчера", file);
+        JOptionPane.showMessageDialog(null, message, "GravitLauncher", JOptionPane.ERROR_MESSAGE);
+    }
+
+    public static void errorHandleAlert(Throwable e)
+    {
+        String message = String.format("Произошла серьезная ошибка при инициализации интерфейса лаунчера.\nДля пользователей:\nОбратитесь к администрации своего проекта с скриншотом этого окна\nJava %d (x%d) Ошибка %s\nОписание: %s\nБолее подробную информацию можно получить из лога", JVMHelper.JVM_VERSION, JVMHelper.JVM_BITS, e.getClass().getName(), e.getMessage() == null ? "null" : e.getMessage());
         JOptionPane.showMessageDialog(null, message, "GravitLauncher", JOptionPane.ERROR_MESSAGE);
     }
 
