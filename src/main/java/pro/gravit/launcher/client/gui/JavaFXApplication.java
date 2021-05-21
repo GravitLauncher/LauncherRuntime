5294719aef86f260c30502776a2395f839ff1db4
Gravita <gravita@gravit.pro>
2021-05-21 17:28:29 +0700

    [FIX] java.lang.NoClassDefFoundError: pro/gravit/utils/enfs/dir/FileEntry

--- src/main/java/pro/gravit/launcher/client/gui/JavaFXApplication.java
+++ src/main/java/pro/gravit/launcher/client/gui/JavaFXApplication.java
@@ -18,6 +18,7 @@ import pro.gravit.launcher.client.gui.commands.VersionCommand;
 import pro.gravit.launcher.client.gui.config.GuiModuleConfig;
 import pro.gravit.launcher.client.gui.config.RuntimeSettings;
 import pro.gravit.launcher.client.gui.config.StdSettingsManager;
+import pro.gravit.launcher.client.gui.helper.EnFSHelper;
 import pro.gravit.launcher.client.gui.impl.*;
 import pro.gravit.launcher.client.gui.overlays.AbstractOverlay;
 import pro.gravit.launcher.client.gui.scenes.AbstractScene;
@@ -35,7 +36,6 @@ import pro.gravit.launcher.request.websockets.StdWebSocketService;
 import pro.gravit.utils.command.BaseCommandCategory;
 import pro.gravit.utils.command.CommandHandler;
 import pro.gravit.utils.enfs.EnFS;
-import pro.gravit.utils.enfs.dir.CipherFile;
 import pro.gravit.utils.enfs.dir.FileEntry;
 import pro.gravit.utils.enfs.dir.URLFile;
 import pro.gravit.utils.helper.IOHelper;
@@ -140,32 +140,7 @@ public class JavaFXApplication extends Application {
         try {
             Class.forName("pro.gravit.utils.enfs.EnFS", false, JavaFXApplication.class.getClassLoader());
             if(runtimeDirectory == null) {
-                LogHelper.error("AAA");
-                enfsDirectory = Paths.get("aone");
-                EnFS.main.newDirectory(enfsDirectory);
-                config.runtime.forEach((name, digest) -> {
-                    EnFS.main.newDirectories(enfsDirectory.resolve(name).getParent());
-                    try {
-                        FileEntry entry;
-                        if(config.runtimeEncryptKey == null) {
-                            entry = new URLFile(Launcher.getResourceURL(name));
-                        } else {
-                            String encodedName = "runtime/"+SecurityHelper.toHex(digest);
-                            entry = new RuntimeCryptedFile(() -> {
-                                try {
-                                    InputStream inputStream = IOHelper.newInput(IOHelper.getResourceURL(encodedName));
-                                    return inputStream;
-                                } catch (IOException e) {
-                                    throw new RuntimeException(e);
-                                }
-                            }, SecurityHelper.fromHex(config.runtimeEncryptKey));
-                        }
-                        EnFS.main.addFile(enfsDirectory.resolve(name), entry);
-                        LogHelper.debug("Pushed %s", enfsDirectory.resolve(name).toString());
-                    } catch (Exception e) {
-                        LogHelper.error(e);
-                    }
-                });
+                enfsDirectory = EnFSHelper.initEnFS(config);
             }
         } catch (Throwable e) {
             if(!(e instanceof ClassNotFoundException)) {
---
+++ src/main/java/pro/gravit/launcher/client/gui/helper/EnFSHelper.java
@@ -0,0 +1,48 @@
+package pro.gravit.launcher.client.gui.helper;
+
+import pro.gravit.launcher.Launcher;
+import pro.gravit.launcher.LauncherConfig;
+import pro.gravit.launcher.client.gui.utils.RuntimeCryptedFile;
+import pro.gravit.utils.enfs.EnFS;
+import pro.gravit.utils.enfs.dir.FileEntry;
+import pro.gravit.utils.enfs.dir.URLFile;
+import pro.gravit.utils.helper.IOHelper;
+import pro.gravit.utils.helper.LogHelper;
+import pro.gravit.utils.helper.SecurityHelper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class EnFSHelper {
+    public static Path initEnFS(LauncherConfig config) throws IOException {
+        LogHelper.error("AAA");
+        Path enfsDirectory = Paths.get("aone");
+        EnFS.main.newDirectory(enfsDirectory);
+        config.runtime.forEach((name, digest) -> {
+            EnFS.main.newDirectories(enfsDirectory.resolve(name).getParent());
+            try {
+                FileEntry entry;
+                if(config.runtimeEncryptKey == null) {
+                    entry = new URLFile(Launcher.getResourceURL(name));
+                } else {
+                    String encodedName = "runtime/"+ SecurityHelper.toHex(digest);
+                    entry = new RuntimeCryptedFile(() -> {
+                        try {
+                            InputStream inputStream = IOHelper.newInput(IOHelper.getResourceURL(encodedName));
+                            return inputStream;
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }, SecurityHelper.fromHex(config.runtimeEncryptKey));
+                }
+                EnFS.main.addFile(enfsDirectory.resolve(name), entry);
+                LogHelper.debug("Pushed %s", enfsDirectory.resolve(name).toString());
+            } catch (Exception e) {
+                LogHelper.error(e);
+            }
+        });
+        return enfsDirectory;
+    }
+}
