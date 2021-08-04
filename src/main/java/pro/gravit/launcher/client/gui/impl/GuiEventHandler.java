eff46f88a8d3d109fa0c8f776ae8ddc927c64230
Tenebrius <magtemtenebrius@gmail.com>
2021-08-04 22:32:50 +0500

    [FIX] AuthRequestEvent fix

--- src/main/java/pro/gravit/launcher/client/gui/impl/GuiEventHandler.java
+++ src/main/java/pro/gravit/launcher/client/gui/impl/GuiEventHandler.java
@@ -2,7 +2,6 @@ package pro.gravit.launcher.client.gui.impl;
 
 import pro.gravit.launcher.client.gui.JavaFXApplication;
 import pro.gravit.launcher.client.gui.scenes.login.LoginScene;
-import pro.gravit.launcher.events.NotificationEvent;
 import pro.gravit.launcher.events.RequestEvent;
 import pro.gravit.launcher.events.request.AuthRequestEvent;
 import pro.gravit.launcher.request.WebSocketEvent;
@@ -19,14 +18,14 @@ public class GuiEventHandler implements ClientWebSocketService.EventHandler {
     @Override
     public <T extends WebSocketEvent> boolean eventHandle(T event) {
         LogHelper.dev("Processing event %s", event.getType());
-        if( event instanceof RequestEvent)
-        {
-            if(!((RequestEvent) event).requestUUID.equals(RequestEvent.eventUUID))
+        if (event instanceof RequestEvent) {
+            if (!((RequestEvent) event).requestUUID.equals(RequestEvent.eventUUID))
                 return false;
         }
         try {
             if (event instanceof AuthRequestEvent) {
                 boolean isNextScene = application.getCurrentScene() instanceof LoginScene;
+                ((LoginScene) application.getCurrentScene()).isLoginStarted = true;
                 LogHelper.dev("Receive auth event. Send next scene %s", isNextScene ? "true" : "false");
                 application.stateService.setAuthResult(null, (AuthRequestEvent) event);
                 if (isNextScene && ((LoginScene) application.getCurrentScene()).isLoginStarted)
