f899f1940d17808d14a9255ea3e377603058bd34
Gravita <gravita@gravit.pro>
2022-12-22 21:20:19 +0700

    [FEATURE] Java 17

--- src/main/java/pro/gravit/launcher/client/JavaRuntimeModule.java
+++ src/main/java/pro/gravit/launcher/client/JavaRuntimeModule.java
@@ -30,7 +30,7 @@ public class JavaRuntimeModule extends LauncherModule {
     private RuntimeProvider provider;
 
     public JavaRuntimeModule() {
-        super(new LauncherModuleInfo("StdJavaRuntime", new Version(2, 1, 3, 1, Version.Type.STABLE),
+        super(new LauncherModuleInfo("StdJavaRuntime", new Version(2, 1, 3, 1, Version.Type.EXPERIMENTAL),
                 0, new String[]{}, new String[]{"runtime"}));
     }
 
