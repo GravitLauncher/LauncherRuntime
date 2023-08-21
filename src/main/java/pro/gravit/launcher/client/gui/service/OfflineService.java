c9e44acd67fc18aba2d30e4ab1c19acba12a14d2
microwin7 <usa.microwin8@gmail.com>
2023-08-22 02:45:24 +0300

    [FEATURE] Correct random username

--- src/main/java/pro/gravit/launcher/client/gui/service/OfflineService.java
+++ src/main/java/pro/gravit/launcher/client/gui/service/OfflineService.java
@@ -51,7 +51,7 @@ public class OfflineService {
                         login = oAuthPassword.accessToken;
                     }
                     if (login == null) {
-                        login = "Player";
+                        login = "Player%s".formatted(100 + (int) (Math.random() * ((999 - 100) + 1)));
                     }
                     return new AuthRequestEvent(
                             permissions,
