de079a6a2adb88c5c4c1ce514277aa893bb9a4af
Gravita <12893402+gravit0@users.noreply.github.com>
2023-11-02 19:29:31 +0700

    [FIX] Server description

--- src/main/java/pro/gravit/launcher/client/gui/scenes/serverinfo/ServerInfoScene.java
+++ src/main/java/pro/gravit/launcher/client/gui/scenes/serverinfo/ServerInfoScene.java
@@ -4,6 +4,7 @@ import javafx.application.Platform;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBase;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
@@ -73,8 +74,10 @@ public class ServerInfoScene extends AbstractScene {
         avatar.setImage(originalAvatarImage);
         ClientProfile profile = application.profilesService.getProfile();
         LookupHelper.<Label>lookupIfPossible(layout, "#serverName").ifPresent((e) -> e.setText(profile.getTitle()));
-        LookupHelper.<Label>lookupIfPossible(layout, "#serverDescription")
-                    .ifPresent((e) -> e.setText(profile.getInfo()));
+        LookupHelper.<ScrollPane>lookupIfPossible(layout, "#serverDescriptionPane").ifPresent((e) -> {
+            var label = (Label) e.getContent();
+            label.setText(profile.getInfo());
+        });
         LookupHelper.<Label>lookupIfPossible(layout, "#nickname")
                     .ifPresent((e) -> e.setText(application.authService.getUsername()));
         Pane serverButtonContainer = LookupHelper.lookup(layout, "#serverButton");
